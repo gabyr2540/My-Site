fe9b99eb38fa0aecacee86e40748395d208dd898
Girls Who Code 13 <gwc13@AD.CS.FIU.EDU>
2018-07-11 11:25:34 -0400

    first commit

---
+++ myScript.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("myScript");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+
+  x[slideIndex-1].style.display = "block";
+}
+}
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function bigImg(x) {
+    x.style.height = "500px";
+    x.style.width = "600px";
+}
+
+function normalImg(x) {
+    x.style.height = "300px";
+    x.style.width = "400px";
+}
